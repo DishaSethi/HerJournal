6fc11089db95fd8f83b613c360e698c445ffb2f3
Disha <sethidisha123@gmail.com>
2024-09-03 20:27:31 +0530

    udateblog feature done

--- src/frontendRoutes/homeRoutes.js
+++ src/frontendRoutes/homeRoutes.js
@@ -26,8 +26,9 @@ router.get('/blogs/:id', async(req,res)=>{
             Authorization:`Bearer ${token}`
         }});
         const blog=response.data;
+        const isAuthenticated=req.cookies.token ? true: false;
         console.log('Blog data:',blog);
-        res.render('blog',{blog});
+        res.render('blog',{blog,isAuthenticated});
     }catch(error){
         console.log(error);
         res.status(500).json({message:error.message});
@@ -52,7 +53,8 @@ router.post('/delete/:id',async(req,res)=>{
             res.redirect('/profile');
         }else{
             console.log('Delete failed:',response.data.message);
-            res.render('blog',{error:'Failed to delete blog',blog:response.data.blog});
+            const isAuthenticated=req.cookies.token ? true: false;
+            res.render('blog',{error:'Failed to delete blog',blog:response.data.blog,isAuthenticated});
         }
     } catch (error){
       console.log('Error deleting blog:',error);
@@ -60,5 +62,60 @@ router.post('/delete/:id',async(req,res)=>{
         message:'Error deleting blog'
       });
     }
+});
+
+router.get('/update/:id',async(req,res)=>{
+    const blogId=req.params.id;
+    const token=req.cookies.token;
+    if(!token){
+        res.render('login');
+    }
+    try{
+        const response=await axios.get(`http://localhost:5000/api/blogs/${blogId}`,{
+            withCredentials:true,
+            headers:{
+                Authorization:`Bearer ${token}`
+            }
+    });
+
+    res.render('update',{blog:response.data});
+    }catch(error){
+        console.log('Error fetching blog data',error);
+        const isAuthenticated=req.cookies.token ? true: false;
+        res.render('blog',{error:'Failed to load blog for editing',isAuthenticated});
+    }
+});
+
+
+router.post('/update/:id',async(req,res)=>{
+try{
+    const blogId=req.params.id;
+    const token=req.cookies.token;
+
+    const response=await axios.put(`http://localhost:5000/api/blogs/${blogId}`,{
+        title:req.body.title,
+        content:req.body.content,
+    
+    },{
+        withCredentials:true,
+        headers:{
+            Authorization:`Bearer ${token}`
+        }
+    });
+    const isAuthenticated=req.cookies.token ? true: false;
+    if(response.data.message=='Blog updated successfully'){
+        console.log(response.data.message);
+
+        res.redirect(`/blogs/${blogId}`);
+    }else{
+        console.log('Updates failed:',response.data.message);
+        res.render('blog',{error:'Failed to update blog',blog:response.data.blog,isAuthenticated});
+    }
+} catch(error){
+    console.log('Error updating blog:',error);
+    res.status(500).json({
+      message:'Error updating blog'
+    });   
+}
 });
 module.exports=router;
