17c7c1d75c4d0a3bbe3b01027c2042774e71dc06
Disha <sethidisha123@gmail.com>
2024-09-13 23:25:55 +0530

    readme done

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
+My major goal of this project was to learn implementing REST APIs , Sockets, JWT Cookies-based authentication by building backend and learning to call APIs from the Frontend using Axios.
+All of this has been achieved so far, currently I am working on some minor changes left and Frontend UI and adding additional feautures. An ongoing Blog Platform project where:
+ Features: 
+ 1.Create, Read, Update, Delete a blog 
+ 2. Authorization and Authentication 
+3. Real-Time Commenting System.
 
+Tech Stack Used: 
+Frontend 
+HTML CSS JS EJS 
+Backend: 
+NODE JS EXPRESS JS MONGO DB 
+Real time Commenting System: 
+Sockets 
+Authentication/ Authorization:
+ JWT
--- app.js
+++ app.js
@@ -2,20 +2,35 @@ const express=require('express');
 const app=express();
 const path=require('path');
 const session = require('express-session');
+const cookieParser=require('cookie-parser');
+const MongoStore = require('connect-mongo');
+const cors = require('cors');
 
+app.use(express.static(path.join(__dirname,'../public')));
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
 
-const loginRoutes=require('./routes/loginRoutes');
-const homeRoutes=require('./routes/homeRoutes');
-const profileRoutes=require('./routes/profileRoutes');
+const loginRoutes=require('./src/frontendRoutes/loginRoutes');
+const homeRoutes=require('./src/frontendRoutes/homeRoutes');
+const profileRoutes=require('./src/frontendRoutes/profileRoutes');
+
+app.use(cookieParser());
+app.use(cors({
+    origin:'http://localhost:3000',
+    credentials:true,
+    // allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+
+
 
 app.use(session({
-    secret:'your_secret_key',
-    resave:false,
-    saveUninitialized:true,
-    cookie:{secure:false}
+    secret: 'your_secret_key',
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: 'mongodb://localhost:27017/blog-platform' }),
+    cookie: { secure: false,httpOnly:true,sameSite:'lax' } // Set to true in production
 }));
 
 app.use(express.urlencoded({extended:true}));
@@ -23,6 +38,7 @@ app.use(express.urlencoded({extended:true}));
 app.use('/',homeRoutes);
 app.use('/',loginRoutes);
 app.use('/',profileRoutes);
+
 
 const PORT=process.env.PORT || 3000;
 
