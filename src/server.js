82512d55f210d17114eb489697003ca41d50e4c4
DishaSethi <sethidisha123@gmail.com>
2026-04-08 22:14:20 +0530

    changed some settings in server.js

--- src/server.js
+++ src/server.js
@@ -24,6 +24,8 @@ const connectDB = require('./config/db');
 connectDB();
 
 const app = express();
+app.set('trust proxy', 1);
+
 const server=http.createServer(app);
 const io=  socketIo(server);
 app.locals.API_URL = process.env.API_URL ;
@@ -37,11 +39,16 @@ app.use((req, res, next) => {
     console.log('Cookies:', req.cookies); // Log all cookies to verify
     next();
 });
-
+const allowedOrigins = ['http://localhost:3000', process.env.CLIENT_URL];
 app.use(cors({
-    origin:'http://localhost:3000',
-    credentials:true,
-    // allowedHeaders: ['Content-Type', 'Authorization']
+    origin: function(origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
 }));
 
 app.use(bodyParser.json());
@@ -110,6 +117,6 @@ socket.on('disconnect',()=>{
 
 
  
- console.log(process.env.MONGODB_URI);
+console.log("MongoDB URI loaded:", process.env.MONGODB_URI ? "Yes" : "No");
 const PORT=process.env.PORT||5000;
-server.listen(PORT,()=> console.log(`Server running on port ${PORT} `));
+server.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
